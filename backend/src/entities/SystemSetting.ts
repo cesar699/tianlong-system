06815b9c83b1e32c3eb69f0b2cb5648d24e4457a
cesar699 <hcp@163.com>
2025-05-06 17:49:20 +0800

    Add files via upload

---
+++ backend/src/entities/SystemSetting.ts
@@ -0,0 +1,17 @@
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class SystemSetting {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    key: string;
+
+    @Column()
+    value: string;
+
+    @Column({ default: '' })
+    description: string;
+}
